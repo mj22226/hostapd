807f74dd5a498543316b3329189dc2d249bd1c70
Ilan Peer <ilan.peer@intel.com>
2026-02-19 22:24:58 +0200

    NAN: Security processing on NDP done
    When a Secure Association (SA) NDP is done, keys should only be
    installed if the cipher suite used is stronger and newer than any of the
    cipher suites used for already established NDPs.
    
    As the NAN specification only allows a single key for a given pair of
    initiator/responder NDIs, add logic to determine if the newly
    established keys should be installed.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/nan/nan_i.h
+++ src/nan/nan_i.h
@@ -270,6 +270,32 @@ struct nan_elem_container_entry {
 };
 
 /**
+ * struct nan_peer_sec_info_entry - NAN peer security information entry
+ *
+ * Maintains the latest security information for an NDI pair.
+ *
+ * @list: Used for linking in the peer security info list
+ *	(struct nan_peer_info::sec)
+ * @peer_ndi: Peer NDI address
+ * @local_ndi: Local NDI address
+ * @csid: Cipher Suite ID used for the secure NAN communication
+ * @pmk: PMK shared with the peer
+ * @pmkid: PMKID shared with the peer
+ * @ptk: PTK shared with the peer
+ */
+struct nan_peer_sec_info_entry {
+	struct dl_list list;
+
+	u8 peer_ndi[ETH_ALEN];
+	u8 local_ndi[ETH_ALEN];
+
+	enum nan_cipher_suite_id csid;
+	u8 pmk[PMK_LEN];
+	u8 pmkid[PMKID_LEN];
+	struct nan_ptk ptk;
+};
+
+/**
  * struct nan_peer_info - NAN peer information
  *
  * @last_seen: Timestamp of the last update of the peer info
@@ -279,6 +305,8 @@ struct nan_elem_container_entry {
  *	(struct nan_dev_capa_entry::list entries)
  * @element_container: List of element container entries of the peer
  *	(struct nan_elem_container_entry::list entries)
+ * @sec: List of security information entries of the peer
+ *	(struct nan_peer_sec_info_entry::list entries)
  */
 struct nan_peer_info {
 	struct os_reltime last_seen;
@@ -286,6 +314,7 @@ struct nan_peer_info {
 	struct dl_list avail_entries;
 	struct dl_list dev_capa;
 	struct dl_list element_container;
+	struct dl_list sec;
 };
 
 /**
@@ -582,5 +611,7 @@ int nan_sec_add_attrs(struct nan_data *nan, struct nan_peer *peer,
 int nan_sec_init_resp(struct nan_data *nan, struct nan_peer *peer);
 int nan_sec_pre_tx(struct nan_data *nan, struct nan_peer *peer,
 		   struct wpabuf *buf);
+bool nan_sec_ndp_store_keys(struct nan_data *nan, struct nan_peer *peer,
+			    const u8 *peer_ndi, const u8 *local_ndi);
 
 #endif /* NAN_I_H */
