sailfishos-mirror/hostap

commit 015a47221fd497ab36d31c6807611383dfdf0af9

P2P: Track STA-connected DFS AP information for assisted DFS
Track and synchronize DFS AP connection state between the STA interface
and P2P module. When a STA is connected to a DFS AP, this information is
stored and made available to the P2P module, enabling P2P GO to operate
on the same DFS channel without performing its own Channel Availability
Check (CAC).

The implementation validates that the connected AP is a legitimate DFS
owner (not a P2P GO) and enforces regulatory requirements by checking
for FCC domain and matching country codes between the device and the AP.
Support for Multi-Link Operation (MLO) is included to handle DFS
connections on any link.

This enables P2P group formation on DFS channels by leveraging the
existing STA connection's DFS monitoring and CAC completion.

Signed-off-by: Shivani Baranwal <shivbara@qti.qualcomm.com>

wpa_supplicant/events.c
@@ -396,6 +396,7 @@ void wpa_supplicant_mark_disassoc(struct wpa_supplicant *wpa_s)
 	sme_clear_on_disassoc(wpa_s);
 	wpa_s->current_bss = NULL;
 	wpa_s->assoc_freq = 0;
+	wpa_s->assisted_dfs = false;
 
 	if (bssid_changed)
 		wpas_notify_bssid_changed(wpa_s);
@@ -3578,6 +3579,62 @@ static void wpas_parse_connection_info(struct wpa_supplicant *wpa_s,
 }
 
 
+static bool is_assisted_dfs_p2p_allowed_cc(const char *country)
+{
+	return country[0] == 'U' && country[1] == 'S';
+}
+
+
+static bool is_assisted_p2p_dfs_allowed(struct wpa_supplicant *wpa_s,
+					struct wpa_bss *bss)
+{
+	const u8 *elem;
+	const char *country;
+
+	if (wpa_s->hw_dfs_domain != HOSTAPD_DFS_REGION_FCC ||
+	    !wpa_s->device_country_set)
+		return false;
+
+	/* Country IE (alpha2 at first two bytes of the payload) */
+	elem = wpa_bss_get_ie(bss, WLAN_EID_COUNTRY);
+	if (!elem || elem[1] < 2)
+		return false;
+
+	country = (const char *) (elem + 2);
+
+	/* Require the BSS country to match the device country */
+	if (country[0] != wpa_s->device_country[0] ||
+	    country[1] != wpa_s->device_country[1])
+		return false;
+
+	return is_assisted_dfs_p2p_allowed_cc(country);
+}
+
+
+static bool is_dfs_owner_ap(struct wpa_supplicant *wpa_s, struct wpa_bss *bss)
+{
+	if (!bss)
+		return false;
+
+	/* Must NOT have P2P IE (not a P2P GO) */
+	if (wpa_bss_get_vendor_ie(bss, P2P_IE_VENDOR_TYPE) ||
+	    wpa_bss_get_vendor_ie_beacon(bss, P2P_IE_VENDOR_TYPE))
+		return false;
+
+	/* Must be an infrastructure mode connection, not P2P group */
+	if (wpa_s->current_ssid && wpa_s->current_ssid->p2p_group)
+		return false;
+
+	/* Beacon interval of the connected DFS AP needs to be short enough to
+	 * have time to inform P2P clients about the need to vacate the
+	 * operating channel within regulatory requirements. */
+	if (bss->beacon_int == 0 || bss->beacon_int > 100)
+		return false;
+
+	return true;
+}
+
+
 static int wpa_supplicant_event_associnfo(struct wpa_supplicant *wpa_s,
 					  union wpa_event_data *data)
 {
@@ -3932,6 +3989,70 @@ no_pfs:
 	}
 
 	wpa_s->assoc_freq = data->assoc_info.freq;
+
+#ifdef CONFIG_P2P
+	if (wpa_s->allow_p2p_assisted_dfs &&
+	    ieee80211_is_dfs(wpa_s->assoc_freq, wpa_s->hw.modes,
+			     wpa_s->hw.num_modes)) {
+		struct wpa_bss *bss = NULL;
+
+		if (bssid_known)
+			bss = wpa_bss_get_bssid_latest(wpa_s, bssid);
+
+		if (bss && is_assisted_p2p_dfs_allowed(wpa_s, bss) &&
+		    is_dfs_owner_ap(wpa_s, bss)) {
+			enum chan_width ap_operation_chan_width =
+				CHAN_WIDTH_UNKNOWN;
+			struct ieee802_11_elems resp_elems_local;
+
+			/* Parse resp_ies locally to extract AP operation
+			 * channel width */
+			if (data->assoc_info.resp_ies &&
+			    ieee802_11_parse_elems(
+				    data->assoc_info.resp_ies,
+				    data->assoc_info.resp_ies_len,
+				    &resp_elems_local, 0) != ParseFailed)
+				ap_operation_chan_width =
+					get_operation_channel_width(
+						&resp_elems_local);
+
+			wpa_s->assisted_dfs = true;
+			wpas_update_dfs_ap_info(wpa_s, wpa_s->assoc_freq,
+						ap_operation_chan_width, false);
+		}
+	} else if (wpa_s->allow_p2p_assisted_dfs && wpa_s->valid_links) {
+		/* For MLO, check other links as well for DFS */
+		int i;
+
+		for_each_link(wpa_s->valid_links, i) {
+			struct wpa_bss *link_bss;
+
+			if (wpa_s->links[i].freq == wpa_s->assoc_freq)
+				continue;
+
+			if (ieee80211_is_dfs(wpa_s->links[i].freq,
+					     wpa_s->hw.modes,
+					     wpa_s->hw.num_modes)) {
+				link_bss = wpa_s->links[i].bss;
+				if (!link_bss)
+					link_bss = wpa_bss_get_bssid(
+						wpa_s, wpa_s->links[i].bssid);
+
+				if (link_bss &&
+				    is_assisted_p2p_dfs_allowed(wpa_s,
+								link_bss) &&
+				    is_dfs_owner_ap(wpa_s, link_bss)) {
+					wpa_s->assisted_dfs = true;
+					wpas_update_dfs_ap_info(
+						wpa_s, wpa_s->links[i].freq,
+						wpa_s->links[i].channel_bandwidth,
+						false);
+					break;
+				}
+			}
+		}
+	}
+#endif /* CONFIG_P2P */
 
 #ifndef CONFIG_NO_ROBUST_AV
 	wpas_handle_assoc_resp_qos_mgmt(wpa_s, data->assoc_info.resp_ies,
@@ -4877,6 +4998,8 @@ static void wpa_supplicant_event_disassoc_finish(struct wpa_supplicant *wpa_s,
 	wpa_sm_notify_disassoc(wpa_s->wpa);
 	ptksa_cache_flush(wpa_s->ptksa, wpa_s->bssid, WPA_CIPHER_NONE);
 
+	wpas_update_dfs_ap_info(wpa_s, 0, 0, true);
+
 	if (locally_generated)
 		wpa_s->disconnect_reason = -reason_code;
 	else
@@ -5529,6 +5652,13 @@ void wpa_supplicant_update_channel_list(struct wpa_supplicant *wpa_s,
 
 		wpa_printf(MSG_DEBUG, "%s: Updating hw mode",
 			   ifs->ifname);
+		if (info && info->alpha2[0] &&
+		    info->type == REGDOM_TYPE_COUNTRY) {
+			ifs->device_country[0] = info->alpha2[0];
+			ifs->device_country[1] = info->alpha2[1];
+			ifs->device_country[2] = '\0';
+			ifs->device_country_set = true;
+		}
 		free_hw_features(ifs);
 		ifs->hw.modes = wpa_drv_get_hw_feature_data(
 			ifs, &ifs->hw.num_modes, &ifs->hw.flags, &dfs_domain);

src/p2p/p2p.c
@@ -3075,6 +3075,22 @@ void p2p_set_dev_addr(struct p2p_data *p2p, const u8 *addr)
 }
 
 
+void p2p_update_dfs_ap_info(struct p2p_data *p2p, int freq,
+			    enum chan_width ap_ch_width, bool disconnect_evt)
+{
+	if (disconnect_evt) {
+		p2p->dfs_ap_connected = false;
+		p2p->sta_connected_freq = 0;
+		p2p->sta_connected_chan_width = CHAN_WIDTH_UNKNOWN;
+		return;
+	}
+
+	p2p->sta_connected_freq = freq;
+	p2p->sta_connected_chan_width = ap_ch_width;
+	p2p->dfs_ap_connected = true;
+}
+
+
 static void p2p_pairing_info_deinit(struct p2p_data *p2p)
 {
 #ifdef CONFIG_PASN

src/p2p/p2p.h
@@ -2817,4 +2817,7 @@ int p2p_pasn_get_ptk(struct p2p_data *p2p, const u8 **buf, size_t *buf_len);
 void p2p_usd_service_hash(struct p2p_data *p2p, const char *service_name);
 int p2p_get_dira_info(struct p2p_data *p2p, char *buf, size_t buflen);
 
+void p2p_update_dfs_ap_info(struct p2p_data *p2p, int freq,
+			    enum chan_width ap_ch_width, bool disconnect_evt);
+
 #endif /* P2P_H */

src/p2p/p2p_i.h
@@ -695,6 +695,25 @@ struct p2p_data {
 
 	bool usd_service;
 	u8 p2p_service_hash[P2PS_HASH_LEN];
+
+	/**
+	 * dfs_ap_connected - Whether STA is connected to a DFS AP
+	 */
+	bool dfs_ap_connected;
+
+	/**
+	 * sta_connected_freq - STA connection frequency
+	 *
+	 * Valid only when dfs_ap_connected is true.
+	 */
+	int sta_connected_freq;
+
+	/**
+	 * sta_connected_chan_width - STA connection channel width
+	 *
+	 * Valid only when dfs_ap_connected is true.
+	 */
+	enum chan_width sta_connected_chan_width;
 };
 
 /**

wpa_supplicant/p2p_supplicant.c
@@ -2792,6 +2792,7 @@ wpas_p2p_init_group_interface(struct wpa_supplicant *wpa_s, int go)
 
 	wpas_p2p_clone_config(group_wpa_s, wpa_s);
 	group_wpa_s->p2p2 = wpa_s->p2p2;
+	group_wpa_s->assisted_dfs = wpa_s->assisted_dfs;
 
 	if (wpa_s->conf->p2p_interface_random_mac_addr) {
 		if (wpa_drv_set_mac_addr(group_wpa_s,
@@ -5801,6 +5802,7 @@ int wpas_p2p_mac_setup(struct wpa_supplicant *wpa_s)
 int wpas_p2p_init(struct wpa_global *global, struct wpa_supplicant *wpa_s)
 {
 	struct p2p_config p2p;
+	struct wpa_supplicant *t_wpa_s;
 	int i;
 
 	if (wpa_s->conf->p2p_disabled)
@@ -6023,6 +6025,16 @@ int wpas_p2p_init(struct wpa_global *global, struct wpa_supplicant *wpa_s)
 	if (global->p2p == NULL)
 		return -1;
 	global->p2p_init_wpa_s = wpa_s;
+
+	for (t_wpa_s = global->ifaces; t_wpa_s; t_wpa_s = t_wpa_s->next) {
+		if (global->p2p && t_wpa_s->dfs_ap_connected) {
+			p2p_update_dfs_ap_info(
+				global->p2p, t_wpa_s->sta_connected_freq,
+				t_wpa_s->sta_connected_chan_width,
+				false);
+			break;
+		}
+	}
 
 	for (i = 0; i < MAX_WPS_VENDOR_EXT; i++) {
 		if (wpa_s->conf->wps_vendor_ext[i] == NULL)

wpa_supplicant/wpa_supplicant.c
@@ -7835,6 +7835,7 @@ static int wpa_supplicant_init_iface(struct wpa_supplicant *wpa_s,
 						      &wpa_s->hw.num_modes,
 						      &wpa_s->hw.flags,
 						      &dfs_domain);
+	wpa_s->hw_dfs_domain = dfs_domain;
 	if (wpa_s->hw.modes) {
 		u16 i;
 
@@ -10180,4 +10181,26 @@ int wpas_get_owe_trans_network(const u8 *owe_ie, const u8 **bssid,
 #else /* CONFIG_OWE */
 	return -1;
 #endif /* CONFIG_OWE */
+}
+
+
+void wpas_update_dfs_ap_info(struct wpa_supplicant *wpa_s, int freq,
+			     enum chan_width ap_ch_width,
+			     bool disconnect_evt)
+{
+	if (disconnect_evt) {
+		wpa_printf(MSG_DEBUG, "Disconnect event of DFS AP");
+		wpa_s->sta_connected_freq = 0;
+		wpa_s->sta_connected_chan_width = CHAN_WIDTH_UNKNOWN;
+	} else {
+		wpa_s->sta_connected_freq = freq;
+		wpa_s->sta_connected_chan_width = ap_ch_width;
+	}
+	wpa_s->dfs_ap_connected = !disconnect_evt;
+
+#ifdef CONFIG_P2P
+	if (wpa_s->global->p2p)
+		p2p_update_dfs_ap_info(wpa_s->global->p2p, freq, ap_ch_width,
+				       disconnect_evt);
+#endif /* CONFIG_P2P */
 }

wpa_supplicant/wpa_supplicant_i.h
@@ -718,6 +718,7 @@ struct wpa_supplicant {
 	unsigned char own_addr[ETH_ALEN];
 	unsigned char perm_addr[ETH_ALEN];
 	char ifname[100];
+	u8 hw_dfs_domain;
 #ifdef CONFIG_MATCH_IFACE
 	int matched;
 #endif /* CONFIG_MATCH_IFACE */
@@ -1639,6 +1640,13 @@ struct wpa_supplicant {
 	bool last_scan_non_coloc_6ghz;
 	bool support_6ghz;
 	bool allow_p2p_assisted_dfs;
+	char device_country[3];
+	bool device_country_set;
+	bool assisted_dfs;
+
+	bool dfs_ap_connected;
+	int sta_connected_freq;
+	enum chan_width sta_connected_chan_width;
 
 	struct wpa_signal_info last_signal_info;
 
@@ -2092,5 +2100,8 @@ bool wpas_ap_supports_rsn_overriding_2(struct wpa_supplicant *wpa_s,
 				       struct wpa_bss *bss);
 int wpas_get_owe_trans_network(const u8 *owe_ie, const u8 **bssid,
 			       const u8 **ssid, size_t *ssid_len);
+void wpas_update_dfs_ap_info(struct wpa_supplicant *wpa_s, int freq,
+			     enum chan_width ap_ch_width,
+			     bool disconnect_evt);
 
 #endif /* WPA_SUPPLICANT_I_H */

mj22226/hostapd

commit 150c64b59c19d21ffe7cbe4bf989cb9a1999afce

P2P: Fix compilation issue due to unused functions in non-P2P builds
These helper functions are called only from within a CONFIG_P2P block.

Fixes: 015a47221fd4 ("P2P: Track STA-connected DFS AP information for assisted DFS")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/events.c
@@ -3579,6 +3579,8 @@ static void wpas_parse_connection_info(struct wpa_supplicant *wpa_s,
 }
 
 
+#ifdef CONFIG_P2P
+
 static bool is_assisted_dfs_p2p_allowed_cc(const char *country)
 {
 	return country[0] == 'U' && country[1] == 'S';
@@ -3633,6 +3635,8 @@ static bool is_dfs_owner_ap(struct wpa_supplicant *wpa_s, struct wpa_bss *bss)
 
 	return true;
 }
+
+#endif /* CONFIG_P2P */
 
 
 static int wpa_supplicant_event_associnfo(struct wpa_supplicant *wpa_s,