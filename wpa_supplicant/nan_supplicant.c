cschuber/hostap

commit 10262547cf14af0194bf508c22d30b86d087a89c

NAN: Keep a list of known peers and their attributes
Keep a list of known peers as this would be needed to support future
flows, e.g., NAN Data Path establishment.

This commit doesn't process the attributes associated with the added
peer. This would be done in later commits.

Signed-off-by: Ilan Peer <ilan.peer@intel.com>

wpa_supplicant/nan_supplicant.c
@@ -681,9 +681,18 @@ void wpas_nan_de_rx_sdf(struct wpa_supplicant *wpa_s, const u8 *src,
 			const u8 *a3, unsigned int freq,
 			const u8 *buf, size_t len, int rssi)
 {
+	bool store_peer;
+
 	if (!wpa_s->nan_de)
 		return;
-	nan_de_rx_sdf(wpa_s->nan_de, src, a3, freq, buf, len, rssi);
+
+	store_peer = nan_de_rx_sdf(wpa_s->nan_de, src, a3, freq, buf,
+				   len, rssi);
+
+	if (!wpas_nan_ready(wpa_s) || !store_peer)
+		return;
+
+	nan_add_peer(wpa_s->nan, src, buf, len);
 }
 
 

src/nan/nan.c
@@ -11,6 +11,8 @@
 #include "nan.h"
 #include "nan_i.h"
 
+#define NAN_MAX_PEERS 32
+
 
 struct nan_data * nan_init(const struct nan_config *cfg)
 {
@@ -29,15 +31,41 @@ struct nan_data * nan_init(const struct nan_config *cfg)
 		return NULL;
 	}
 
+	dl_list_init(&nan->peer_list);
+
 	wpa_printf(MSG_DEBUG, "NAN: Initialized");
 
 	return nan;
 }
 
 
+static void nan_del_peer(struct nan_data *nan, struct nan_peer *peer)
+{
+	if (!peer)
+		return;
+
+	wpa_printf(MSG_DEBUG, "NAN: Removing peer: " MACSTR,
+		   MAC2STR(peer->nmi_addr));
+
+	dl_list_del(&peer->list);
+	os_free(peer);
+}
+
+
+static void nan_peer_clear_all(struct nan_data *nan)
+{
+	struct nan_peer *peer, *n_peer;
+
+	dl_list_for_each_safe(peer, n_peer, &nan->peer_list,
+			      struct nan_peer, list)
+		nan_del_peer(nan, peer);
+}
+
+
 void nan_deinit(struct nan_data *nan)
 {
 	wpa_printf(MSG_DEBUG, "NAN: Deinit");
+	nan_peer_clear_all(nan);
 	os_free(nan->cfg);
 	os_free(nan);
 }
@@ -96,6 +124,7 @@ void nan_flush(struct nan_data *nan)
 	}
 
 	nan->nan_started = 0;
+	nan_peer_clear_all(nan);
 }
 
 
@@ -110,4 +139,77 @@ void nan_stop(struct nan_data *nan)
 
 	nan_flush(nan);
 	nan->cfg->stop(nan->cfg->cb_ctx);
+}
+
+
+struct nan_peer * nan_get_peer(struct nan_data *nan, const u8 *addr)
+{
+	struct nan_peer *peer;
+
+	dl_list_for_each(peer, &nan->peer_list, struct nan_peer, list) {
+		if (ether_addr_equal(peer->nmi_addr, addr))
+			return peer;
+	}
+
+	return NULL;
+}
+
+
+static struct nan_peer * nan_alloc_peer(struct nan_data *nan)
+{
+	struct nan_peer *peer, *oldest = NULL;
+	size_t count = 0;
+
+	dl_list_for_each(peer, &nan->peer_list, struct nan_peer, list) {
+		count++;
+		if (!oldest ||
+		    os_reltime_before(&peer->last_seen, &oldest->last_seen))
+			oldest = peer;
+	}
+
+	if (count >= NAN_MAX_PEERS && oldest) {
+		wpa_printf(MSG_DEBUG,
+			   "NAN: Remove peer=" MACSTR " to make room",
+			   MAC2STR(oldest->nmi_addr));
+
+		nan_del_peer(nan, oldest);
+	}
+
+	peer = os_zalloc(sizeof(*peer));
+	if (!peer)
+		return NULL;
+
+	dl_list_add(&nan->peer_list, &peer->list);
+	return peer;
+}
+
+
+int nan_add_peer(struct nan_data *nan, const u8 *addr,
+		 const u8 *device_attrs, size_t device_attrs_len)
+{
+	struct nan_peer *peer;
+
+	/* Allow adding peer devices even if NAN was not started, to support
+	 * discovery during USD, etc. */
+	if (!nan)
+		return -1;
+
+	/* TODO: parse the device attributes to update the peer information */
+	if (!device_attrs || !device_attrs_len) {
+		wpa_printf(MSG_DEBUG,
+			   "NAN: Ignore add_peer with no device attributes");
+		return -1;
+	}
+
+	peer = nan_get_peer(nan, addr);
+	if (!peer) {
+		peer = nan_alloc_peer(nan);
+		if (!peer)
+			return -1;
+
+		os_memcpy(peer->nmi_addr, addr, ETH_ALEN);
+	}
+
+	os_get_reltime(&peer->last_seen);
+	return 0;
 }

src/nan/nan.h
@@ -45,4 +45,7 @@ int nan_update_config(struct nan_data *nan,
 void nan_stop(struct nan_data *nan);
 void nan_flush(struct nan_data *nan);
 
+int nan_add_peer(struct nan_data *nan, const u8 *addr,
+		 const u8 *device_attrs, size_t device_attrs_len);
+
 #endif /* NAN_H */

src/nan/nan_i.h
@@ -9,11 +9,34 @@
 #ifndef NAN_I_H
 #define NAN_I_H
 
+#include "list.h"
+
 struct nan_config;
 
+/**
+ * struct nan_peer - Represents a known NAN peer
+ * @list: List node for linking peers
+ * @nmi_addr: NMI of the peer
+ * @last_seen: Timestamp of the last time this peer was seen
+ */
+struct nan_peer {
+	struct dl_list list;
+	u8 nmi_addr[ETH_ALEN];
+	struct os_reltime last_seen;
+};
+
+/**
+ * struct nan_data - Internal data structure for NAN
+ * @cfg: Pointer to the NAN configuration structure
+ * @nan_started: Flag indicating if NAN has been started
+ * @peer_list: List of known peers
+ */
 struct nan_data {
 	struct nan_config *cfg;
 	u8 nan_started:1;
+	struct dl_list peer_list;
 };
+
+struct nan_peer * nan_get_peer(struct nan_data *nan, const u8 *addr);
 
 #endif /* NAN_I_H */

mj22226/hostapd

commit b246ade12f99e9b63d8e40b6403f4df8e7ce459b

NAN: Fix CONFIG_NAN_USD build without CONFIG_NAN
Fixes: 10262547cf14 ("NAN: Keep a list of known peers and their attributes")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/nan_supplicant.c
@@ -711,10 +711,15 @@ void wpas_nan_de_rx_sdf(struct wpa_supplicant *wpa_s, const u8 *src,
 	store_peer = nan_de_rx_sdf(wpa_s->nan_de, src, a3, freq, buf,
 				   len, rssi);
 
-	if (!wpas_nan_ready(wpa_s) || !store_peer)
+	if (!store_peer)
+		return;
+
+#ifdef CONFIG_NAN
+	if (!wpas_nan_ready(wpa_s))
 		return;
 
 	nan_add_peer(wpa_s->nan, src, buf, len);
+#endif /* CONFIG_NAN */
 }
 
 