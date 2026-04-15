sailfishos-mirror/hostap

commit eff8a482285a03264cc566fb958b32547a28971e

NAN: Validation of peer availability
After processing the NDL related attributes, and before the flow
continues to handle the data, validate that the availability information
is valid:

1. Parse the availability attributes.
2. Validate that the immutable schedule is covered by the
   availability attributes.
3. Validate that the NDC schedule is covered by the
   availability attributes.

Signed-off-by: Ilan Peer <ilan.peer@intel.com>

src/nan/nan_util.c
@@ -1128,3 +1128,99 @@ bool nan_sched_covered_by_avail_entry(struct nan_data *nan,
 
 	return ret == 1;
 }
+
+
+static int
+nan_sched_bf_covered_by_avail_entries(struct nan_data *nan,
+				      struct dl_list *avail_entries,
+				      const struct bitfield *sched_bf,
+				      u8 map_id)
+{
+	struct nan_avail_entry *avail;
+	struct bitfield *res_bf = NULL;
+	int ret = 0;
+
+	dl_list_for_each(avail, avail_entries, struct nan_avail_entry,
+			 list) {
+		struct bitfield *avail_bf;
+
+		if (avail->map_id != map_id)
+			continue;
+
+		/* Schedule can only be covered by committed/conditional */
+		if (avail->type != NAN_AVAIL_ENTRY_CTRL_TYPE_COMMITTED &&
+		    avail->type != NAN_AVAIL_ENTRY_CTRL_TYPE_COND)
+			continue;
+
+		/* Convert the availability entry to bitfield */
+		avail_bf = nan_tbm_to_bf(nan, &avail->tbm);
+		if (!avail_bf) {
+			ret = -1;
+			goto fail;
+		}
+
+		bitfield_dump(avail_bf, "NAN: Availability entry bitmap");
+		if (!res_bf) {
+			res_bf = avail_bf;
+		} else {
+			struct bitfield *tmp_bf;
+
+			tmp_bf = bitfield_union(res_bf, avail_bf);
+			bitfield_free(avail_bf);
+
+			if (!tmp_bf) {
+				ret = -1;
+				goto fail;
+			}
+
+			bitfield_free(res_bf);
+			res_bf = tmp_bf;
+		}
+	}
+
+	ret = bitfield_is_subset(res_bf, sched_bf);
+fail:
+	wpa_printf(MSG_DEBUG,
+		   "NAN: Is bitfield schedule subset of entries=%d", ret);
+	bitfield_free(res_bf);
+	return ret;
+}
+
+
+/**
+ * nan_sched_covered_by_avail_entries - Check if schedule is covered by the
+ * list of availability attributes
+ *
+ * @nan: NAN module context from nan_init()
+ * @avail_entries: A list of availability entries (see &struct nan_avail_entry)
+ * @sched: An array with 0 or more &struct nan_sched_entry entries
+ * @sched_len: Length of the &sched array
+ * Returns: 1 of schedule is covered by the entries; 0 if not and -1 on error
+ */
+int nan_sched_covered_by_avail_entries(struct nan_data *nan,
+				       struct dl_list *avail_entries,
+				       const u8 *sched, size_t sched_len)
+{
+	struct dl_list sched_entries;
+	struct bitfield *sched_bf;
+	u8 map_id;
+	int ret;
+
+	if (!sched || !sched_len)
+		return 1;
+
+	dl_list_init(&sched_entries);
+	ret = nan_sched_entries_to_avail_entries(nan, &sched_entries,
+						 sched, sched_len);
+
+	sched_bf = nan_sched_to_bf(nan, &sched_entries, &map_id);
+	nan_flush_avail_entries(&sched_entries);
+
+	ret = nan_sched_bf_covered_by_avail_entries(nan, avail_entries,
+						    sched_bf, map_id);
+
+	wpa_printf(MSG_DEBUG, "NAN: NDC schedule is %sa subset of entries",
+		   ret == 1 ? "" : "NOT ");
+	bitfield_free(sched_bf);
+	return ret;
+}

src/nan/nan_i.h
@@ -453,5 +453,8 @@ struct bitfield * nan_sched_to_bf(struct nan_data *nan, struct dl_list *sched,
 bool nan_sched_covered_by_avail_entry(struct nan_data *nan,
 				      struct nan_avail_entry *avail,
 				      struct bitfield *sched_bf, u8 map_id);
+int nan_sched_covered_by_avail_entries(struct nan_data *nan,
+				       struct dl_list *avail_entries,
+				       const u8 *sched, size_t sched_len);
 
 #endif /* NAN_I_H */

src/nan/nan_ndl.c
@@ -191,6 +191,38 @@ static struct nan_ndl * nan_ndl_alloc(struct nan_data *nan)
 }
 
 
+static int nan_ndl_validate_peer_avail(struct nan_data *nan,
+				       struct nan_peer *peer)
+{
+	struct nan_ndl *ndl = peer->ndl;
+	int ret;
+
+	/* First, validate if immutable is covered by the availability map */
+	ret = nan_sched_covered_by_avail_entries(nan, &peer->info.avail_entries,
+						 ndl->immut_sched,
+						 ndl->immut_sched_len);
+	if (ret <= 0) {
+		wpa_printf(MSG_DEBUG,
+			   "NAN: Peer avail: Immutable is not covered by avail");
+		return -1;
+	}
+
+	/* Now validate NDC schedule is covered by the availability map */
+	ret = nan_sched_covered_by_avail_entries(nan, &peer->info.avail_entries,
+						 ndl->ndc_sched,
+						 ndl->ndc_sched_len);
+	if (ret <= 0) {
+		wpa_printf(MSG_DEBUG,
+			   "NAN: Peer avail: NDC is not covered by avail");
+		return -1;
+	}
+
+	wpa_printf(MSG_DEBUG,
+		   "NAN: NDL: Peer NDC and immutable are covered by avail");
+	return 0;
+}
+
+
 static enum nan_ndl_status nan_ndl_res_status(struct nan_data *nan,
 					      struct nan_peer *peer)
 {
@@ -402,6 +434,7 @@ static int nan_ndl_attr_handle_req(struct nan_data *nan, struct nan_peer *peer,
 				   const struct ndl_attr_params *params)
 {
 	struct nan_ndl *ndl;
+	int ret;
 
 	wpa_printf(MSG_DEBUG, "NAN: NDL: Handle request");
 
@@ -463,6 +496,10 @@ static int nan_ndl_attr_handle_req(struct nan_data *nan, struct nan_peer *peer,
 		ndl->immut_sched_len = params->immut_sched_len;
 	}
 
+	ret = nan_ndl_validate_peer_avail(nan, peer);
+	if (!ret)
+		goto fail;
+
 	nan_ndl_set_state(nan, ndl, NAN_NDL_STATE_REQ_RECV);
 
 	wpa_printf(MSG_DEBUG, "NAN: NDL: Handle request done");
@@ -478,6 +515,7 @@ static int nan_ndl_attr_handle_resp(struct nan_data *nan, struct nan_peer *peer,
 				    const struct ndl_attr_params *params)
 {
 	struct nan_ndl *ndl = peer->ndl;
+	int ret;
 
 	wpa_printf(MSG_DEBUG, "NAN: NDL: Handle response");
 
@@ -552,6 +590,7 @@ static int nan_ndl_attr_handle_resp(struct nan_data *nan, struct nan_peer *peer,
 		if (!ndl->ndc_sched) {
 			wpa_printf(MSG_INFO,
 				   "NAN: NDL: Resp: Failed to allocate NDC schedule");
+			ret = -1;
 			goto fail;
 		}
 
@@ -572,10 +611,15 @@ static int nan_ndl_attr_handle_resp(struct nan_data *nan, struct nan_peer *peer,
 		if (!ndl->immut_sched) {
 			wpa_printf(MSG_INFO,
 				   "NAN: NDL: Resp: fail allocate immutable schedule");
+			ret = -1;
 			goto fail;
 		}
 		ndl->immut_sched_len = params->immut_sched_len;
 	}
+
+	ret = nan_ndl_validate_peer_avail(nan, peer);
+	if (!ret)
+		goto fail;
 
 	wpa_printf(MSG_DEBUG, "NAN: NDL: Resp: status=%u", params->status);
 
@@ -590,7 +634,7 @@ fail:
 	ndl->status = NAN_NDL_STATUS_REJECTED;
 	ndl->reason = NAN_REASON_RESOURCE_LIMITATION;
 	ndl->send_naf_on_error = 1;
-	return 0;
+	return ret;
 }
 
 
@@ -598,6 +642,7 @@ static int nan_ndl_attr_handle_conf(struct nan_data *nan, struct nan_peer *peer,
 				    const struct ndl_attr_params *params)
 {
 	struct nan_ndl *ndl = peer->ndl;
+	int ret;
 
 	if (!ndl) {
 		wpa_printf(MSG_DEBUG, "NAN: NDL: Confirm without an NDL");
@@ -664,6 +709,7 @@ static int nan_ndl_attr_handle_conf(struct nan_data *nan, struct nan_peer *peer,
 		if (!ndl->ndc_sched) {
 			wpa_printf(MSG_INFO,
 				   "NAN: NDL: Failed to allocate NDC schedule");
+			ret = -1;
 			goto fail;
 		}
 		ndl->ndc_sched_len = params->ndc_sched_len;
@@ -681,10 +727,15 @@ static int nan_ndl_attr_handle_conf(struct nan_data *nan, struct nan_peer *peer,
 		if (!ndl->immut_sched) {
 			wpa_printf(MSG_INFO,
 				   "NAN: NDL: Failed to allocate immutable schedule");
+			ret = -1;
 			goto fail;
 		}
 		ndl->immut_sched_len = params->immut_sched_len;
 	}
+
+	ret = nan_ndl_validate_peer_avail(nan, peer);
+	if (!ret)
+		goto fail;
 
 	nan_ndl_set_state(nan, ndl, NAN_NDL_STATE_DONE);
 	return 0;
@@ -692,7 +743,7 @@ static int nan_ndl_attr_handle_conf(struct nan_data *nan, struct nan_peer *peer,
 fail:
 	ndl->reason = NAN_REASON_RESOURCE_LIMITATION;
 	ndl->send_naf_on_error = 1;
-	return -1;
+	return ret;
 }
 
 

mj22226/hostapd

commit 41a615af19c3471780bb971e0fc0d1119eb94944

NAN: Fix memory leak in schedule validation
The nan_sched_covered_by_avail_entries() function allocates memory for
sched_entries via nan_sched_entries_to_avail_entries(), but fails to
free it when nan_sched_to_bf() returns NULL. This causes a memory leak.

Add proper cleanup by calling nan_flush_avail_entries() before returning
on error. Also fix the error handling to properly check the return value
of nan_sched_entries_to_avail_entries().

Fixes: eff8a482285a ("NAN: Validation of peer availability")
Fixes: 3c0156b84b9f ("NAN: Propagate rejection reason code from availability validation")
Signed-off-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>

src/nan/nan_util.c
@@ -1285,12 +1285,16 @@ bool nan_sched_covered_by_avail_entries(struct nan_data *nan,
 		return true;
 
 	dl_list_init(&sched_entries);
-	ret = nan_sched_entries_to_avail_entries(nan, &sched_entries,
-						 sched, sched_len);
+	if (nan_sched_entries_to_avail_entries(nan,
+					       &sched_entries,
+					       sched, sched_len))
+		return false;
 
 	sched_bf = nan_sched_to_bf(nan, &sched_entries, &map_id, &reason);
-	if (!sched_bf)
+	if (!sched_bf) {
+		nan_flush_avail_entries(&sched_entries);
 		return false;
+	}
 
 	nan_flush_avail_entries(&sched_entries);
 