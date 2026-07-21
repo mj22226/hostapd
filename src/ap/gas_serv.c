b28ebdfb15c548ab3f7ad2114e90956dbdb79dc1
Jouni Malinen <jouni.malinen@oss.qualcomm.com>
2026-07-21 16:14:23 +0300

    ANQP: Skip ANQP-elements that would not fit into GAS response
    If the total length of ANQP-elements cannot fit into a maximum GAS
    response (128 * gas_frag_limit octets), there is no point in trying to
    build the full response. Instead, skip ANQP-elements that would not fit
    and send the other ones. This allows a response to be sent out and the
    debug log entries might make it a bit clearer why the AP configuration
    is such that it has too much ANQP data to be usable in practice.
    
    Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

--- src/ap/gas_serv.c
+++ src/ap/gas_serv.c
@@ -890,6 +890,7 @@ gas_serv_build_gas_resp_payload(struct hostapd_data *hapd,
 	struct wpabuf *buf;
 	size_t len;
 	unsigned int i;
+	size_t prev_len, max_len = 128 * hapd->conf->gas_frag_limit;
 
 	len = 1400;
 	if (request & (ANQP_REQ_NAI_REALM | ANQP_REQ_NAI_HOME_REALM))
@@ -906,67 +907,237 @@ gas_serv_build_gas_resp_payload(struct hostapd_data *hapd,
 
 	if (request & ANQP_REQ_CAPABILITY_LIST)
 		anqp_add_capab_list(hapd, &buf);
-	if (request & ANQP_REQ_VENUE_NAME)
+
+	if (request & ANQP_REQ_VENUE_NAME) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_venue_name(hapd, &buf);
-	if (request & ANQP_REQ_EMERGENCY_CALL_NUMBER)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_EMERGENCY_CALL_NUMBER) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_elem(hapd, &buf, ANQP_EMERGENCY_CALL_NUMBER);
-	if (request & ANQP_REQ_NETWORK_AUTH_TYPE)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+	if (request & ANQP_REQ_NETWORK_AUTH_TYPE) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_network_auth_type(hapd, &buf);
-	if (request & ANQP_REQ_ROAMING_CONSORTIUM)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+	if (request & ANQP_REQ_ROAMING_CONSORTIUM) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_roaming_consortium(hapd, &buf);
-	if (request & ANQP_REQ_IP_ADDR_TYPE_AVAILABILITY)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+	if (request & ANQP_REQ_IP_ADDR_TYPE_AVAILABILITY) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_ip_addr_type_availability(hapd, &buf);
-	if (request & (ANQP_REQ_NAI_REALM | ANQP_REQ_NAI_HOME_REALM))
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+	if (request & (ANQP_REQ_NAI_REALM | ANQP_REQ_NAI_HOME_REALM)) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_nai_realm(hapd, &buf, home_realm, home_realm_len,
 				   request & ANQP_REQ_NAI_REALM,
 				   request & ANQP_REQ_NAI_HOME_REALM);
-	if (request & ANQP_REQ_3GPP_CELLULAR_NETWORK)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_3GPP_CELLULAR_NETWORK) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_3gpp_cellular_network(hapd, &buf);
-	if (request & ANQP_REQ_AP_GEOSPATIAL_LOCATION)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_AP_GEOSPATIAL_LOCATION) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_elem(hapd, &buf, ANQP_AP_GEOSPATIAL_LOCATION);
-	if (request & ANQP_REQ_AP_CIVIC_LOCATION)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_AP_CIVIC_LOCATION) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_elem(hapd, &buf, ANQP_AP_CIVIC_LOCATION);
-	if (request & ANQP_REQ_AP_LOCATION_PUBLIC_URI)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_AP_LOCATION_PUBLIC_URI) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_elem(hapd, &buf, ANQP_AP_LOCATION_PUBLIC_URI);
-	if (request & ANQP_REQ_DOMAIN_NAME)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_DOMAIN_NAME) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_domain_name(hapd, &buf);
-	if (request & ANQP_REQ_EMERGENCY_ALERT_URI)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_EMERGENCY_ALERT_URI) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_elem(hapd, &buf, ANQP_EMERGENCY_ALERT_URI);
-	if (request & ANQP_REQ_TDLS_CAPABILITY)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_TDLS_CAPABILITY) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_elem(hapd, &buf, ANQP_TDLS_CAPABILITY);
-	if (request & ANQP_REQ_EMERGENCY_NAI)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_EMERGENCY_NAI) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_elem(hapd, &buf, ANQP_EMERGENCY_NAI);
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
 
 	for (i = 0; i < num_extra_req; i++) {
+		prev_len = wpabuf_len(buf);
 #ifdef CONFIG_FILS
 		if (extra_req[i] == ANQP_FILS_REALM_INFO) {
 			anqp_add_fils_realm_info(hapd, &buf);
+			if (wpabuf_len(buf) > max_len) {
+				wpa_printf(MSG_INFO,
+					   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+				buf->used = prev_len;
+			}
 			continue;
 		}
 #endif /* CONFIG_FILS */
 		if (extra_req[i] == ANQP_VENUE_URL) {
 			anqp_add_venue_url(hapd, &buf);
+			if (wpabuf_len(buf) > max_len) {
+				wpa_printf(MSG_INFO,
+					   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+				buf->used = prev_len;
+			}
 			continue;
 		}
 		anqp_add_elem(hapd, &buf, extra_req[i]);
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
 	}
 
 #ifdef CONFIG_HS20
-	if (request & ANQP_REQ_HS_CAPABILITY_LIST)
+	if (request & ANQP_REQ_HS_CAPABILITY_LIST) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_hs_capab_list(hapd, &buf);
-	if (request & ANQP_REQ_OPERATOR_FRIENDLY_NAME)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_OPERATOR_FRIENDLY_NAME) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_operator_friendly_name(hapd, &buf);
-	if (request & ANQP_REQ_WAN_METRICS)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_WAN_METRICS) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_wan_metrics(hapd, &buf);
-	if (request & ANQP_REQ_CONNECTION_CAPABILITY)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_CONNECTION_CAPABILITY) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_connection_capability(hapd, &buf);
-	if (request & ANQP_REQ_OPERATING_CLASS)
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
+
+	if (request & ANQP_REQ_OPERATING_CLASS) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_operating_class(hapd, &buf);
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
 #endif /* CONFIG_HS20 */
 
 #ifdef CONFIG_MBO
-	if (request & ANQP_REQ_MBO_CELL_DATA_CONN_PREF)
+	if (request & ANQP_REQ_MBO_CELL_DATA_CONN_PREF) {
+		prev_len = wpabuf_len(buf);
 		anqp_add_mbo_cell_data_conn_pref(hapd, &buf);
+		if (wpabuf_len(buf) > max_len) {
+			wpa_printf(MSG_INFO,
+				   "ANQP: ANQP-element would go beyond maximum GAS response length - skip");
+			buf->used = prev_len;
+		}
+	}
 #endif /* CONFIG_MBO */
 
 	return buf;
